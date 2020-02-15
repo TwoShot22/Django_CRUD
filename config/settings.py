a3db29fbac928a4d176dc41b473ce38f894c30a4
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-15 22:22:39 +0900

    Add Classcrud & Viewcrud App

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'classcrud.apps.ClasscrudConfig',
+    'viewcrud.apps.ViewcrudConfig',
 ]
 
 MIDDLEWARE = [
