6c930dfbbbe783db875439d12acc2f8dcfb4b443
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-18 19:30:06 +0900

    classcrud/Views.py & admin.py Updated

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 import viewcrud.views
 import viewcrud.urls
-# import classcrud.views
-# import classcrud.urls
+import classcrud.views
+import classcrud.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', viewcrud.views.welcome, name="welcome"),
     path('funccrud/', include('viewcrud.urls')),
-    # path('classcrud/', include('classcrud.urls')),
+    path('classcrud/', include('classcrud.urls')),
 ]
