697c8877f6e76a5b486f69d769b1124baafa25d9
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-02-19 20:23:09 +0900

    Add Template for Generic View

--- classcrud/views.py
+++ classcrud/views.py
@@ -8,22 +8,22 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView # Manag
 from .models import ClassBlog
 
 
-class BlogView(ListView):
+class BlogView(ListView):   # HTML Template : Blog Lists -> (소문자모델)_list.html
     model = ClassBlog
 
-class BlogCreate(CreateView):
-    model = ClassBlog
-    fields = ['title', 'body']
-    success_url = reverse_lazy('list')
-
-class BlogDetail(DetailView):
-    model = ClassBlog
-
-class BlogUpdate(UpdateView):
+class BlogCreate(CreateView):   # HTML Template : Input Form for Create -> (소문자모델)_form.html
     model = ClassBlog
     fields = ['title', 'body']
     success_url = reverse_lazy('list')
 
-class BlogDelete(DeleteView):
+class BlogDetail(DetailView):   # HTML Template : Detail Page -> (소문자모델)_detail.html
+    model = ClassBlog
+
+class BlogUpdate(UpdateView):   # HTML Template : Input Form for Update -> (소문자모델)_form. html
+    model = ClassBlog
+    fields = ['title', 'body']
+    success_url = reverse_lazy('list')
+
+class BlogDelete(DeleteView):   # HTML Template : Confirm for Delete -> (소문자모델)_confirm_delete.html
     model = ClassBlog
     success_url = reverse_lazy('list')
